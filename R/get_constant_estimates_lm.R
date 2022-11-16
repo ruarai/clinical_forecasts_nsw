8db8f833d00b19b0647c9198c5623012be45d338
Ruarai Tobin <ruarai.tobin@unimelb.edu.au>
2022-11-16 04:32:20 +0000

    use moving window estimates for pr_age and pr_ICU

--- R/get_constant_estimates_lm.R
+++ R/get_constant_estimates_lm.R
@@ -3,6 +3,7 @@ get_constant_estimates_lm <- function(
   nsw_cases,
   hospital_data,
   case_trajectory,
+  time_varying_estimates_moving,
   
   forecast_dates
 ) {
@@ -11,7 +12,7 @@ get_constant_estimates_lm <- function(
   
   require(mgcv)
   
-  fit_period <- c(ymd("2022-02-01"), forecast_dates$hospital_data - days(14))
+  fit_period <- c(ymd("2022-06-01"), forecast_dates$hospital_data - days(14))
   
   
   
@@ -65,143 +66,51 @@ get_constant_estimates_lm <- function(
     mutate(
       age_group = factor(age_group),
       t = as.numeric(date_onset - fit_period[1]),
+      dow = wday(date_onset),
+      
+      weight = 1,
+      
       .before = 2
     )
+  
   
   
   hosp_fits <- map(
     age_groups,
     function(i_age_group) {
       gam(
-        #n_hosp ~ s(t, by = n_cases, k = 10),
+        #n_hosp ~ s(t, by = n_cases, k = 10) + s(dow, k = 5, by = n_cases),
         n_hosp ~ n_cases,
-        data = fit_data %>% filter(age_group == i_age_group)
+        data = fit_data %>% filter(age_group == i_age_group),
+        
+        weights = weight
       )
     }
   ) %>%
     `names<-`(age_groups)
   
   
-  total_hospitalisations <- hospital_subset %>%
-    drop_na(age_group) %>% 
-    count(age_group, name = "n_hosp")
-  
-  total_ICU_admissions <- hospital_subset %>%
-    filter(ever_in_icu) %>% 
-    drop_na(age_group) %>% 
-    count(age_group, name = "n_ICU")
-  
-  prob_ICU <- total_hospitalisations %>%
-    left_join(total_ICU_admissions, by = "age_group") %>%
-    
-    mutate(pr_ICU = n_ICU / n_hosp,
-           
-           pr_ICU = replace_na(pr_ICU, 0)) %>%
-    
-    select(age_group, pr_ICU)
   
   
+  moving_estimates_with_cases <- time_varying_estimates_moving %>%
+    left_join(case_trajectory %>% select(date = date_onset, n_cases = count)) %>%
+    mutate(n_cases = n_cases * pr_age_given_case)
   
-  
-  
-  
-  
-  cases_fit <- gam(
-    n_cases ~ s(t, k = 15, by = age_group) + age_group,
-    
-    family = nb,
-    data = fit_data
-  )
-  
-  
-  fit_data %>%
-    mutate(pred_n_cases = predict(cases_fit, newdata = .),
-           pred_n_cases = exp(pred_n_cases)) %>%
-    
-    ggplot() +
-    
-    geom_point(aes(x = date_onset, y = n_cases),
-               size = 0.4) +
-    geom_line(aes(x = date_onset, y = pred_n_cases),
-              size = 1,
-              colour = ggokabeito::palette_okabe_ito(5)) +
-    
-    facet_wrap(~age_group, scales = "free_y") +
-    
-    theme_minimal()
-  
-  
-  fit_data %>%
-    mutate(pred_n_cases = predict(cases_fit, newdata = .),
-           pred_n_cases = exp(pred_n_cases)) %>%
-    
-    group_by(date_onset) %>% 
-    mutate(
-      pred_pr_age_given_case = pred_n_cases / sum(pred_n_cases),
-      pr_age_given_case = n_cases / sum(n_cases)
-    ) %>% 
-    
-    ggplot() +
-    
-    geom_point(aes(x = date_onset, y = pr_age_given_case),
-               size = 0.4) +
-    geom_line(aes(x = date_onset, y = pred_pr_age_given_case),
-              size = 1,
-              colour = ggokabeito::palette_okabe_ito(5)) +
-    
-    facet_wrap(~age_group, scales = "free_y") +
-    
-    theme_minimal()
-  
-  
-  
-  
-  time_varying_estimates <- fit_data %>%
-    select(date_onset, t, age_group) %>% 
-    
-    mutate(
-      pred_n_cases = predict(cases_fit, newdata = .),
-      pred_n_cases = exp(pred_n_cases)
-    ) %>%
-    group_by(date_onset) %>%
-    mutate(pr_age_given_case = pred_n_cases / sum(pred_n_cases)) %>%
-    ungroup() %>%
-    select(date_onset, age_group, pr_age_given_case) %>%
-    
-    
-    
-    left_join(prob_ICU) %>%
-    select(date = date_onset, age_group, pr_age_given_case, pr_ICU) %>%
-    ungroup() %>%
-    
-    complete(
-      age_group = age_groups,
-      date = seq(forecast_dates$simulation_start, forecast_dates$forecast_horizon, by = "days")
-    ) %>%
-    group_by(age_group) %>%
-    arrange(date) %>%
-    fill(pr_age_given_case, pr_ICU, .direction = "downup") %>%
-    ungroup() %>%
-    
-    left_join(case_trajectory %>% rename(n_cases = count, date = date_onset), by = "date") %>% 
-    
-    mutate(
-      n_cases = n_cases * pr_age_given_case
-    )
   
   time_varying_estimates_hosp <- map_dfr(
     age_groups,
     function(i_age_group) {
-      time_varying_estimates %>%
+      moving_estimates_with_cases %>%
         filter(age_group == i_age_group) %>%
-        mutate(t = as.numeric(date - fit_period[1])) %>% 
+        mutate(t = as.numeric(date - fit_period[1]),
+               dow = wday(date),
+               t = if_else(date >= fit_period[2], as.numeric(fit_period[2] - fit_period[1]), t)) %>%
         mutate(pred_n_hosp = predict(hosp_fits[[i_age_group]], newdata = .),
                pr_hosp = pred_n_hosp / n_cases,
                pr_hosp = pmin(pmax(pr_hosp, 0), 1)) %>%
-        select(age_group, date, pr_age_given_case, pr_hosp, pr_ICU)
+        select(bootstrap, age_group, date, pr_age_given_case, pr_hosp, pr_ICU)
     }
-  ) %>%
-    mutate(bootstrap = 1)
+  )
   
   
   time_varying_estimates_hosp
